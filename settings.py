26bdac6cc46d0be840c6bdef0350fc8a324561d0
Dieter <dieter.dieleman@gmail,com>
2024-12-01 07:07:57 +0100

    adding the settingsfile and included it into gitignore

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+
+DEBUG = True
+
+SECRET_KEY = "super-geheime-sleutel"
+
+DATABASE = {
+    'ENGINE': 'sqlite3',
+    'NAME': 'ratama.db'
+}
+
+API_KEY = "mijn-echte-api-key"
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
