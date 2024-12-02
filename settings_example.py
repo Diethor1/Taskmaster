6bac9fbacac70df94d4fd030fc399e17d56f6990
DieterDieleman <159649586+Diethor1@users.noreply.github.com>
2024-12-02 20:13:59 +0100

    Update settings_example.py
    Completing the settings file.

--- settings_example.py
+++ settings_example.py
@@ -2,25 +2,30 @@
 
 import os
 
-# Vul hier je bestandsnaam in 
+# This is the path to your file. It is important, that they are in the same folder as your script
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Adres van je database bestand
+# THe path to your database, here,'data' is the subfolder and 'database.db' the name of your database
 DATABASE_PATH = os.path.join(BASE_DIR, 'data', 'database.db')
 
-# adres van de csv bestanden
+# Path to the csv files. Again, 'Data' is the subdirectory and 'users.csv' etc are examples of csv fileneames
 CSV_FILES = {
     'users': os.path.join(BASE_DIR, 'data', 'users.csv'),
     'products': os.path.join(BASE_DIR, 'data', 'products.csv'),
     'sales': os.path.join(BASE_DIR, 'data', 'sales.csv'),
 }
 
-#Debug modus (True als je nog aan het ontwikkelen bent, False qls je wil produceren.)
+#Debug modus (True, try if you are still developing, False if you want to produce.)
 DEBUG = True
 
-
+#Small dictionary with information about your database
 DATABASE = {
     'ENGINE': 'sqlite3',
     'NAME': 'database_used_for_project.db'
 }
+# If there is an API Key you can add it here
+API_KEY = "my api key"
+
+# Sepcify Host information
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
